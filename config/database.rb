0cbe33cda25adccafdd0419259461f123acbe0c4
Alice Post <alicep2322@gmail.com>
2020-01-06 21:08:24 -0700

    Update connection adapter for postgresql

--- config/database.rb
+++ config/database.rb
@@ -3,6 +3,6 @@ database_name = "set-list-#{SetList.environment}"
 
 # connect ActiveRecord with the current database
 ActiveRecord::Base.establish_connection(
-  :adapter  => "sqlite3",
+  :adapter  => "postgresql",
   :database => "db/#{database_name}",
 )
